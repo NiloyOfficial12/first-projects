a9fc186c16b0f83f882e8eae3378dbbc6b8ce090
Niloy Saha <your.email@example.com>
2026-06-11 05:35:56 +0600

    added my actual code

---
+++ Main.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val project drainage = "ClassWave Live"
+    println("Hello, GitHub!")
+    println("Kotlin code is running successfully for $project!")
+    
+    val a = 10
+    val b = 20
+    val sum = a + b
+    println("The sum of $a and $b is: $sum")
+}
