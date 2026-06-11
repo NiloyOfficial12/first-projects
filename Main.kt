04e249b7459bf6aff7f9073508510c9b069e6cbe
Niloy Saha <your.email@example.com>
2026-06-11 15:41:08 +0600

    right code

--- Main.kt
+++ Main.kt
@@ -4,9 +4,7 @@ fun main() {
     println("Kotlin code is running successfully for $project!")
     
     val a = 10
-    val b = 20
-    val c = 23
-    val d = 34
-    val sum = a + b + c + d
-    println("The sum of $a and $b and $c and &d is: $sum")
+    val b = 15
+    val sum = a + b
+    println("The sum of $a and $b is: $sum")
 }
