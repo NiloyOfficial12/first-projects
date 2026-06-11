6f3865e25799cfb7e21f465f1751e211eb2de767
NiloyOfficial12 <niloysaha1532@gmail.com>
2026-06-11 15:33:23 +0600

    Add variable d and update sum calculation

--- Main.kt
+++ Main.kt
@@ -6,6 +6,7 @@ fun main() {
     val a = 10
     val b = 20
     val c = 23
-    val sum = a + b + c
-    println("The sum of $a and $b and $c is: $sum")
+    val d = 34
+    val sum = a + b + c + d
+    println("The sum of $a and $b and $c and &d is: $sum")
 }
